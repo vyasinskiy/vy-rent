88b504911d1bd75f45231c25ecfe9c4b1f847cff
vyasinskiy <yasinsky93@gmail.com>
2022-08-18 15:40:51 +0400

    Add mongo atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
 import { ApiService } from './api.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
   controllers: [AppController],
   providers: [AuthService, ApiService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MongoClient } from 'mongodb';
 
 declare const module: any;
 
+// Replace <password> with actual password!
+const uri =
+  'mongodb+srv://Vitaliy:<password>@cluster0.vjfia.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+
 async function bootstrap() {
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db('kv-online-db');
+  const collection = db.collection('kv');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
